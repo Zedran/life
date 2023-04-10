50480856fea89e8f74bf4bcb07f8b86ea77f7aaa
Zedran <17592740+Zedran@users.noreply.github.com>
2023-04-10 14:56:45 +0200

    mk.sh now aborts the building process on tests fail

--- mk.sh
+++ mk.sh
@@ -5,8 +5,19 @@ if [ ! -d "$build_dir" ]; then
     mkdir $build_dir
 fi
 
+failed=0
+
 for package in ./src ./src/world; do
     go test -v $package
+    
+    if [ $? -ne 0 ]; then
+        failed=1
+    fi
 done
+
+if [ $failed -eq 1 ]; then
+    echo -e "\nTests failed. Aborting build.\n"
+    exit 1
+fi
 
 go build -trimpath -ldflags "-s -w" -o "$build_dir/$exe_fname" ./src
