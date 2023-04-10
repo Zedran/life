f2e87e288337795fc2e6ff34a50757ecbf1f3437
Zedran <17592740+Zedran@users.noreply.github.com>
2023-04-10 02:07:46 +0200

    world.World: add the Update method

--- src/world/world.go
+++ src/world/world.go
@@ -1,5 +1,11 @@
 package world
 
+/* The size of the cell map border that is omitted during World.Update.
+   Skipping one row allows to avoid the need for ensuring that the
+   iteration over cell map always stays within the bounds of the array.
+*/
+const PADDING int = 1
+
 /* Represents the world of the game - a square grid of cells. */
 type World struct {
 	// The number of cells in one row / column
@@ -22,6 +28,52 @@ func (w *World) Reset() {
 	for i := range w.Cells {
 		w.Cells[i] = 0
 	}
+}
+
+/* Makes the world transition to next generation, updating the cell array. */
+func (w *World) Update() {
+	w.Generation++
+
+	var (
+		pitch  = int(w.Size)
+		buffer = make([]State, len(w.Cells), len(w.Cells))
+	)
+
+	for i := pitch + 1; i <= int(w.Size) - pitch - 1; i += 3  {
+		
+			rowStart := i
+
+			for ; i < rowStart + pitch - 3; i++ {
+				// Neighbour count
+				nc := 
+					w.Cells[i - 1 - pitch] + 
+					w.Cells[i     - pitch] + 
+					w.Cells[i + 1 - pitch] + 
+					w.Cells[i - 1        ] + 
+					w.Cells[i + 1        ] + 
+					w.Cells[i - 1 + pitch] + 
+					w.Cells[i     + pitch] + 
+					w.Cells[i + 1 + pitch]
+
+				switch w.Cells[i] {
+				case ALIVE:
+					for _, lr := range w.Rules.Live {
+						if State(lr) == nc {
+							buffer[i] = ALIVE
+							break
+						}
+					}
+				case DEAD:
+					for _, dr := range w.Rules.Die {
+						if State(dr) == nc {
+							buffer[i] = DEAD
+							break
+						}
+					}
+				}
+			}
+	}
+	w.Cells = buffer
 }
 
 /* Creates new world of specified size. */
