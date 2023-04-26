99155928a6e450fc42cfc59aedef759526b75b8f
Zedran <17592740+Zedran@users.noreply.github.com>
2023-04-26 21:31:09 +0200

    world: add Seed function for seeding Go's PRNG

--- src/world/utils.go
+++ src/world/utils.go
@@ -1,5 +1,12 @@
 package world
 
+import (
+	"crypto/sha256"
+	"math/big"
+	"math/rand"
+	"strconv"
+)
+
 /* Checks whether the slice s contains the specified number n. */
 func Contains(s []uint8, n uint8) bool {
 	for _, e := range s {
@@ -8,4 +15,43 @@ func Contains(s []uint8, n uint8) bool {
 		}
 	}
 	return false
+}
+
+/* Returns the maximum value of int64. */
+func maxInt64() int64 {
+	return int64(^uint64(0) >> 1)
+}
+
+/* Returns sha256sum of s. */
+func sha256sum(s string) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(s))
+	return hash.Sum(nil)
+}
+
+/* 
+	Seeds the PRNG with specified string. Numerical seeds are treated like integers. 
+	For seeds that do not contain digits only, seedFromString is called.
+*/
+func Seed(seed string) {
+	n, err := strconv.ParseInt(seed, 10, 64)
+	if err != nil {
+		n = seedFromString(seed)
+	}
+
+	rand.Seed(n)
+}
+
+/* 
+	Converts seed into int64. It is accomplished in the following way:
+		1. sha256sum of the seed is calculated and saved as big.Int
+		2. big.Int is reduced into int64 range with modulo maxInt64()
+*/
+func seedFromString(seed string) int64 {
+	n := new(big.Int)
+	n.SetBytes(sha256sum(seed))
+
+	n.Mod(n, big.NewInt(maxInt64()))
+	
+	return n.Int64()
 }
