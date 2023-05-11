Zedran/life

commit 087c21bf0747d74047047997367b6f0257e98e1e

Map.Pan: Map offset values are now calculated more accurately

src/map.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math"
+
 	"github.com/hajimehoshi/ebiten/v2"
 
 	"github.com/Zedran/life/src/config"
@@ -182,7 +184,10 @@ func (m *Map) Move(dX, dY float32) {
 
 /* Calls the Map.Move method after translating the movement from graphical measurements into world dimensions. */
 func (m *Map) Pan(dX, dY int) {
-	m.Move(float32(dX) / m.ZoomSteps[m.Zoom], float32(dY) / m.ZoomSteps[m.Zoom])
+	m.Move(
+		float32(math.Round(float64(float32(dX) / m.ZoomSteps[m.Zoom]))), 
+		float32(math.Round(float64(float32(dY) / m.ZoomSteps[m.Zoom]))),
+	)
 }
 
 /* Recalculates the fraction of maximum cell size by which Map.AliveImg and Map.DeadImg are scaled. */

commit 1395ae7a820a0d48dcfee43a906619ecc2418db3

fix Map.Pan bug introduced in 087c21b

src/game.go
@@ -134,6 +134,7 @@ func (g *Game) UpdateDragEvent() {
 	g.DragEvent.Update()
 
 	if !g.DragEvent.Active {
+		g.Map.TruncOffSets()
 		g.DragEvent = nil
 		return
 	}

src/map.go
@@ -185,14 +185,20 @@ func (m *Map) Move(dX, dY float32) {
 /* Calls the Map.Move method after translating the movement from graphical measurements into world dimensions. */
 func (m *Map) Pan(dX, dY int) {
 	m.Move(
-		float32(math.Round(float64(float32(dX) / m.ZoomSteps[m.Zoom]))), 
-		float32(math.Round(float64(float32(dY) / m.ZoomSteps[m.Zoom]))),
+		float32(dX) / m.ZoomSteps[m.Zoom], 
+		float32(dY) / m.ZoomSteps[m.Zoom],
 	)
 }
 
 /* Recalculates the fraction of maximum cell size by which Map.AliveImg and Map.DeadImg are scaled. */
 func (m *Map) RecalculateCellScale() {
 	m.CellScale = float64((m.ZoomSteps[m.Zoom] - BORDER_SIZE) / float32(m.AliveImg.Bounds().Dx()))
+}
+
+/* Called after Game.DragEvent finishes its job to eliminate Map.OffSetX and Map.OffSetY inaccuracies. */
+func (m *Map) TruncOffSets() {
+	m.OffSetX = float32(math.Trunc(float64(m.OffSetX)))
+	m.OffSetY = float32(math.Trunc(float64(m.OffSetY)))
 }
 
 /* Creates new graphical map of the world. */