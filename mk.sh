03a4fb3f73cfd45e4bb9f1fe953b6475e65ad4ad
Zedran <17592740+Zedran@users.noreply.github.com>
2023-05-18 20:56:34 +0200

    prevent repeated messages by aborting mk.sh after first test fail

--- mk.sh
+++ mk.sh
@@ -7,19 +7,13 @@ if [ ! -d "$build_dir" ]; then
     mkdir $build_dir
 fi
 
-failed=0
-
 for package in ./src ./src/world ./src/config ./src/config/theme; do
     go test $package
     
     if [ $? -ne 0 ]; then
-        failed=1
+        echo -e "\nTests failed. Aborting build.\n"
+        exit 1
     fi
 done
-
-if [ $failed -eq 1 ]; then
-    echo -e "\nTests failed. Aborting build.\n"
-    exit 1
-fi
 
 go build -trimpath -ldflags "-s -w -X main.Version=$version" -o "$build_dir/$exe_fname" ./src
