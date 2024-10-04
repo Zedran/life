32cc0a9db2af809e9f517a08de93a4dc4be4f8e2
Zedran <17592740+Zedran@users.noreply.github.com>
2024-10-05 00:49:48 +0200

    Fix unicode icon const declarations

--- internal/ui/icons.go
+++ internal/ui/icons.go
@@ -3,11 +3,11 @@ package ui
 // This group of constants holds "icons" used as labels for buttons
 
 const (
-	ICON_PLAY_TOGGLE  = string(0x25ba) + string(0x007c)
+	ICON_PLAY_TOGGLE  = string('\u25ba') + string('\u007c')
 	ICON_SLOW_DOWN    = "-"
 	ICON_SPEED_UP     = "+"
-	ICON_RESET_STATE  = string(0x00d8)
-	ICON_RANDOM_STATE = string(0x2591) + string(0x2591)
+	ICON_RESET_STATE  = string('\u00d8')
+	ICON_RANDOM_STATE = string('\u2591') + string('\u2591')
 	ICON_FF_I         = "I"
 	ICON_FF_X         = "X"
 	ICON_FF_L         = "L"
