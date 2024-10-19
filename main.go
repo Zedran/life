0cf556e49769c146418458140443a6f7b4f4cc71
Zedran <17592740+Zedran@users.noreply.github.com>
2024-10-20 01:37:18 +0200

    Remove the Version global variable

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-// Holds the short commit of the build
-var Version string
-
 func main() {
-	fmt.Println(Version)
-
 	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
